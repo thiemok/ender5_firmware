61b6d7ac2450d867adcdf34bad7120dadca9904d
Timothy Hoogland <houseofbugs@gmail.com>
2020-01-27 17:27:39 -0600

    Update Configuration_adv.h
    Set control fan speed to 100%. Low speed does not work with all fans.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -51,7 +51,7 @@
   #if ENABLED(USE_CONTROLLER_FAN)
     #define CONTROLLER_FAN_PIN P1_22       // Set a custom pin for the controller fan
     #define CONTROLLERFAN_SECS 60          // Duration in seconds for the fan to run after all motors are disabled
-    #define CONTROLLERFAN_SPEED 204        // 255 == full speed
+    #define CONTROLLERFAN_SPEED 255        // 255 == full speed
   #endif
 #endif
 
