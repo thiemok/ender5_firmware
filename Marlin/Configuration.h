dbf41638100fdaf52c8ccc0d1b8531ac836fed0f
thiemok <krausethiemo@googlemail.com>
2020-02-09 16:25:51 +0100

    feat(Dimensions): updated y dimensions with travel loss after hero me install

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1063,7 +1063,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 225
-#define Y_BED_SIZE 225
+#define Y_BED_SIZE 205
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
